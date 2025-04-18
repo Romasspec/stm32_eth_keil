a0688a6ff03bb0b4b3b5cfc0c084295a399d2f38
Romas <romas140490@gmail.com>
2025-04-18 17:10:14 +0300

    work

--- src/tcp.c
+++ src/tcp.c
@@ -32,36 +32,93 @@ uint8_t tcp_send(uint8_t *ip_addr, uint16_t port, uint8_t op)
 		tcp_pkt->data[2]=0x05;
 		tcp_pkt->data[3]=0x82;
 		tcp_pkt->cs = be16toword(IP_TCP + len);
+		tcp_pkt->cs=checksum((uint8_t*)tcp_pkt-8, len+8);	
+	
+		//Заполним заголовок пакета IP
+		sprintf(str1,"len:%d\r\n", len);
+		uart1_send_buf((uint8_t*)str1, strlen(str1));
+		
+		len+=sizeof(ip_pkt_ptr);
+		sprintf(str1,"len:%d\r\n", len);
+		uart1_send_buf((uint8_t*)str1, strlen(str1));
+		
+		ip_pkt->len=be16toword(len);
+		ip_pkt->id = 0;
+		ip_pkt->ts = 0;
+		ip_pkt->verlen = 0x45;
+		ip_pkt->fl_frg_of=0;
+		ip_pkt->ttl=128;
+		ip_pkt->cs = 0;
+		ip_pkt->prt=IP_TCP;
+		memcpy(ip_pkt->ipaddr_dst,ip_addr,4);
+		memcpy(ip_pkt->ipaddr_src,ipaddr,4);
+		ip_pkt->cs = checksum((void*)ip_pkt,sizeof(ip_pkt_ptr));
+		
+		memcpy(frame->addr_dest,frame->addr_src,6);
+		memcpy(frame->addr_src,macaddr,6);
+		frame->type = ETH_IP;
+		len+=sizeof(enc28j60_frame_ptr);
+		enc28j60_packetSend((void*)frame,len);
+		
+		sprintf(str1,"len:%d\r\n", len);
+		uart1_send_buf((uint8_t*)str1, strlen(str1));
+		uart1_send_buf((uint8_t*)"SYN ACK\r\n",9);
+	
+	} else if (op==TCP_OP_ACK_OF_FIN) {
+		//Заполним заголовок пакета TCP
+		tcp_pkt->port_dst = be16toword(port);
+		tcp_pkt->port_src = be16toword(LOCAL_PORT_TCP);
+		num_seg = tcp_pkt->num_ask;
+		tcp_pkt->num_ask = be32todword(be32todword(tcp_pkt->bt_num_seg) + 1);
+		tcp_pkt->bt_num_seg = num_seg;
+		tcp_pkt->fl = TCP_ACK;
+		tcp_pkt->size_wnd = be16toword(8192);
+		tcp_pkt->urg_ptr = 0;
+		len = sizeof(tcp_pkt_ptr);
+		tcp_pkt->len_hdr = len << 2;		
+		tcp_pkt->cs = be16toword(IP_TCP + len);
 		tcp_pkt->cs=checksum((uint8_t*)tcp_pkt-8, len+8);
+		
+		//Заполним заголовок пакета IP
+		sprintf(str1,"len:%d\r\n", len);
+		uart1_send_buf((uint8_t*)str1, strlen(str1));
+		
+		len+=sizeof(ip_pkt_ptr);
+		sprintf(str1,"len:%d\r\n", len);
+		uart1_send_buf((uint8_t*)str1, strlen(str1));
+		
+		ip_pkt->len=be16toword(len);
+		ip_pkt->id = 0;
+		ip_pkt->ts = 0;
+		ip_pkt->verlen = 0x45;
+		ip_pkt->fl_frg_of=0;
+		ip_pkt->ttl=128;
+		ip_pkt->cs = 0;
+		ip_pkt->prt=IP_TCP;
+		memcpy(ip_pkt->ipaddr_dst,ip_addr,4);
+		memcpy(ip_pkt->ipaddr_src,ipaddr,4);
+		ip_pkt->cs = checksum((void*)ip_pkt,sizeof(ip_pkt_ptr));
+		
+		memcpy(frame->addr_dest,frame->addr_src,6);
+		memcpy(frame->addr_src,macaddr,6);
+		frame->type = ETH_IP;
+		len+=sizeof(enc28j60_frame_ptr);
+		enc28j60_packetSend((void*)frame,len);
+		
+		sprintf(str1,"len:%d\r\n", len);
+		uart1_send_buf((uint8_t*)str1, strlen(str1));
+		uart1_send_buf((uint8_t*)"ACK OF FIN\r\n",9);
+		
+		tcp_pkt->fl = TCP_FIN|TCP_ACK;
+    len = sizeof(tcp_pkt_ptr);
+    tcp_pkt->cs = be16toword(IP_TCP + len);
+    tcp_pkt->cs=checksum((uint8_t*)tcp_pkt-8, len+8);
+    len+=sizeof(ip_pkt_ptr);
+    len+=sizeof(enc28j60_frame_ptr);
+    enc28j60_packetSend((void*)frame,len);		
 	}
 	
-	//Заполним заголовок пакета IP
-	sprintf(str1,"len:%d\r\n", len);
-	uart1_send_buf((uint8_t*)str1, strlen(str1));
-	
-	len+=sizeof(ip_pkt_ptr);
-	sprintf(str1,"len:%d\r\n", len);
-	uart1_send_buf((uint8_t*)str1, strlen(str1));
-	
-	ip_pkt->len=be16toword(len);
-	ip_pkt->id = 0;
-	ip_pkt->ts = 0;
-	ip_pkt->verlen = 0x45;
-	ip_pkt->fl_frg_of=0;
-	ip_pkt->ttl=128;
-	ip_pkt->cs = 0;
-	ip_pkt->prt=IP_TCP;
-	memcpy(ip_pkt->ipaddr_dst,ip_addr,4);
-	memcpy(ip_pkt->ipaddr_src,ipaddr,4);
-	ip_pkt->cs = checksum((void*)ip_pkt,sizeof(ip_pkt_ptr));
-	
-	memcpy(frame->addr_dest,frame->addr_src,6);
-	memcpy(frame->addr_src,macaddr,6);
-	frame->type = ETH_IP;
-	len+=sizeof(enc28j60_frame_ptr);
-	enc28j60_packetSend((void*)frame,len);
-	
-	res =1;
+	res = 1;
 	return res;
 }
 
@@ -82,6 +139,10 @@ uint8_t tcp_read (enc28j60_frame_ptr *frame, uint16_t len)
 	if (tcp_pkt->fl == TCP_SYN)
 	{
 		res = tcp_send(ip_pkt->ipaddr_src, be16toword(tcp_pkt->port_src), TCP_OP_SYNACK);
+	} else if (tcp_pkt->fl == TCP_ACK) {
+		uart1_send_buf((uint8_t*)"ACK\r\n",5);
+	} else if (tcp_pkt->fl == (TCP_FIN|TCP_ACK)) {
+		res = tcp_send(ip_pkt->ipaddr_src, be16toword(tcp_pkt->port_src), TCP_OP_ACK_OF_FIN);
 	}
 
   return res;
